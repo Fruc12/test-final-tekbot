a535ec35b012d63aa871d438d363b11616aa8f95
Fructueux ESSOU <frucessou@gmail.com>
2025-07-05 17:25:14 +0100

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # test-final-tekbot
+
+## 1- Base de données
+Exécuter le script schema.sql pour créer la base de données (de préférence MySQL). Si les identifiants de connection sont différents de root et '', modifier dans `__init__.py`.
+
+## 2- Créer un environnement virtuel et l'activer
+Se postionner a la racine du projet et exécuter les commandes suivantes
+### - Windows
+```sh
+python -m venv env
+./env/Scripts/activate
+```
+### - Linux
+```sh
+python3 -m venv env
+./venv/bin/activate
+```
+
+## 3- Installer les dépendances
+```sh
+pip install -r requirements.txt
+```
+
+## 4- Lancer le projet
+```sh
+flask --app 'tekbot' run --debug
+```
+
+## 5- Routes :
+- [localhost:5000](localhost:5000) : pour consulter la page (TekBot/templates/index.html)
+- [localhost:5000/set?color={value}](localhost:5000/set?color={value}) : pour stocker la couleur de déchet trié. (value is in [green, yellow, red, blue])
---
+++ TekBot/__init__.py
@@ -0,0 +1,63 @@
+import os
+from flaskext.mysql import MySQL
+from flask import Flask, redirect, render_template, request, url_for
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        MYSQL_DATABASE_USER='root',
+        MYSQL_DATABASE_PASSWORD='',
+        MYSQL_DATABASE_DB='tekbot',
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+    
+        
+    mysql = MySQL()
+    mysql.init_app(app)
+    connection = mysql.connect()
+    cursor = connection.cursor()
+    cursor.execute(" TRUNCATE TABLE colors")
+
+    @app.get("/")
+    def render_page():
+        cursor.execute("""
+            SELECT color, COUNT(*) AS total
+            FROM colors
+            WHERE color IN ('green', 'yellow', 'red', 'blue')
+            GROUP BY color
+        """)
+        colors = cursor.fetchall()
+        
+        green = yellow = red = blue = 0
+        context = {}
+        for color, count in colors :
+            if color == "green" : green = count
+            if color == "yellow" : yellow = count
+            if color == "red" : red = count
+            if color == "blue" : blue = count
+            
+        return render_template("index.html", green=green, yellow=yellow, red=red, blue=blue)
+
+    @app.get("/set")
+    def save_color():
+        color = request.args.get("color")
+        if color in ['green', 'yellow', 'red', 'blue'] :
+            cursor.execute(f"INSERT INTO `colors`(`color`) VALUES ('{color}')")
+            connection.commit()
+        return redirect(url_for("render_page"))
+
+    return app
